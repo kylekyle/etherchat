bd818f5f653b02d4856c2dfa0d929f8cb917437a
Kyle King <kylejking@gmail.com>
2018-03-08 16:19:59 -0500

    set source mac

--- src/main/java/edu/usma/etherchat/MessageSender.java
+++ src/main/java/edu/usma/etherchat/MessageSender.java
@@ -14,20 +14,22 @@ import org.pcap4j.packet.UnknownPacket;
 import org.pcap4j.util.MacAddress;
 
 public class MessageSender implements Runnable {
-    
+
     private String device;
     private final BlockingQueue<String> outgoing = new LinkedBlockingQueue<>();
-    
+
     void open(String device) {
         this.device = device;
         new Thread(this).start();
     }
-    
+
     @Override
     public void run() {
         try {
             PcapNetworkInterface nif = Pcaps.getDevByName(device);
-            
+            System.out.println(nif.getLinkLayerAddresses().get(0));
+            MacAddress address = MacAddress.getByAddress(nif.getLinkLayerAddresses().get(0).getAddress());
+
             try (PcapHandle handle = nif.openLive(EtherChat.SNAPLEN, PcapNetworkInterface.PromiscuousMode.PROMISCUOUS, EtherChat.READ_TIMEOUT);) {
                 Stream.generate(() -> {
                     try {
@@ -40,12 +42,12 @@ public class MessageSender implements Runnable {
                         .forEach(message -> {
                             try {
                                 EthernetPacket.Builder frame = new EthernetPacket.Builder()
+                                        .srcAddr(address)
+                                        .paddingAtBuild(true)
                                         .type(EtherChat.ETHERTYPE)
-                                        .srcAddr(MacAddress.getByName("E4:A4:71:42:68:47"))
                                         .dstAddr(MacAddress.ETHER_BROADCAST_ADDRESS)
-                                        .payloadBuilder(new UnknownPacket.Builder().rawData(message.getBytes()))
-                                        .paddingAtBuild(true);
-                                
+                                        .payloadBuilder(new UnknownPacket.Builder().rawData(message.getBytes()));
+
                                 handle.sendPacket(frame.build());
                             } catch (PcapNativeException | NotOpenException ex) {
                                 Window.alert(ex.getMessage());
@@ -56,7 +58,7 @@ public class MessageSender implements Runnable {
             Window.alert(ex.getMessage());
         }
     }
-    
+
     public void send(String message) {
         outgoing.offer(message);
     }
