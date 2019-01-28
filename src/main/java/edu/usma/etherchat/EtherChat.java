3c40b463b69203a03ab74b3a280da90775775a28
Kyle King <kylejking@gmail.com>
2019-01-28 15:03:39 -0500

    Switch ethertype from 0xCAFE to ARP (0x0806)

--- src/main/java/edu/usma/etherchat/EtherChat.java
+++ src/main/java/edu/usma/etherchat/EtherChat.java
@@ -7,7 +7,7 @@ public class EtherChat {
 
     public static final int SNAPLEN = 65536;
     public static final int READ_TIMEOUT = 10;
-    public static final EtherType ETHERTYPE = new EtherType((short) 0xCAFE, "EtherChat");
+    public static final EtherType ETHERTYPE = new EtherType((short) 0x0806, "EtherChat");
 
     public static void main(String[] args) throws InterruptedException {
         MessageSender sender = new MessageSender();
